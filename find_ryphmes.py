480b403f039c8a010ff2e9a656e6274d24e81a45
Andrew <andr-sitnikov@mail.ru>
2021-10-31 13:47:29 +0300

    Console tool was upgraded
    Former-commit-id: 15478256651c567a8da6ef4993a5fe7b101b4ca2

--- find_ryphmes.py
+++ find_ryphmes.py
@@ -2,23 +2,56 @@ import argparse
 
 def decode(s):
       return s.encode('utf-8', 'surrogateescape').decode('1251')
-parser = argparse.ArgumentParser(description = 'Compex tool for finding ryphms')
+
+
+parser = argparse.ArgumentParser(description =
+                                 '''Compex tool for finding ryphms;
+List of parts of speech
+NOUN    имя существительное             хомяк
+ADJF    имя прилагательное (полное)     хороший
+ADJS    имя прилагательное (краткое)    хорош
+COMP    компаратив                      лучше, получше, выше
+VERB    глагол (личная форма)           говорю, говорит, говорил
+INFN    глагол (инфинитив)              говорить, сказать
+PRTF    причастие (полное)              прочитавший, прочитанная
+PRTS    причастие (краткое)             прочитана
+GRND    деепричастие                    прочитав, рассказывая
+NUMR    числительное                    три, пятьдесят
+ADVB    наречие                         круто
+NPRO    местоимение-существительное	он
+PRED    предикатив                      некогда
+PREP    предлог                         в
+CONJ    союз                            и
+PRCL    частица                         бы, же, лишь
+INTJ    междометие                      ой
+''', formatter_class = argparse.RawTextHelpFormatter)
 
 parser.add_argument('to_find', type = str, help = 'what to to find')
 parser.add_argument('--mean', type = str, help = 'set meaning by fields&synonyms')
 parser.add_argument('--rps', type = str, help = 'remove parts of speech')
 
-args = parser.parse_args() # ["сло'во", "--mean", "Battle"]
-args.to_find = decode(args.to_find)
-args.mean = decode(args.mean)
+debug = True
 
+if debug:
+      args = ["сло'во", "--mean", "Battle", '--rps', 'NOUN']
+      args = parser.parse_args(args)
+else:
+      args = parser.parse_args()
+      
+if not debug:
+      args.to_find = decode(args.to_find)
+      args.mean = decode(args.mean)
+      args.rps = decode(args.rps)
 
 import finder
 
 to_find = args.to_find
-all_forms_set = finder.filter_remove_parts_of_speech([])
+remove = args.rps.split('+') if args.rps else []
+print(remove)
+all_forms_set = finder.filter_remove_parts_of_speech(remove)
+print('Filtered:', len(all_forms_set))
 best = finder.get_best_by_transcription(to_find, all_forms_set)
-
+print(best)
 field = []
 if args.mean:
       for i in args.mean.split('+'):
